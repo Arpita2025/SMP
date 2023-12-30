cada5836517d5d8167b6c78fdefda4e5c713cba5
Shubham Kumar Pathak <shubhampathak26092002@gmail.com>
2023-12-31 02:03:46 +0530

    error solved

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,7 +1,16 @@
 const jwt =require("jsonwebtoken");
 module.exports =(req,res,next) =>{
     try{
-      const token =req.header("authorization").split(" ")[1];
+    //   const token =req.header("authorization").split(" ")[1];
+    const authHeader = req.header("authorization");
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            throw new Error("Invalid authorization header");
+        }
+
+        const token = authHeader.split(" ")[1];
+
+
+
       const decryptedToken =jwt.verify(token ,process.env.jwt_secret)
       req.body.userId =decryptedToken.userId;
       next();
