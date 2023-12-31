c98c410e4253b8ae940a0983425eebd8c28e1ab3
Shubham Kumar Pathak <shubhampathak26092002@gmail.com>
2024-01-01 03:23:22 +0530

    loaders implementation

--- client/src/index.js
+++ client/src/index.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { ConfigProvider } from 'antd';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ConfigProvider } from "antd";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-   <ConfigProvider theme={{
-    components :{
-      Button :{
-        colorPrimary : '#40513B',
-        colorPrimaryHover :'#40513B',
-        borderRadius :"2px",
-        boxShadow :"none",
-      },
-    },
-    token :{
-      borderRadius :"2px",
-      colorPrimary :"#40513B",
-    }
-  }}>
-    <App />
+  <Provider store={store}>
+    <ConfigProvider
+      theme={{
+        components: {
+          Button: {
+            colorPrimary: "#40513B",
+            colorPrimaryHover: "#40513B",
+            borderRadius: "2px",
+            boxShadow: "none",
+          },
+        },
+        token: {
+          borderRadius: "2px",
+          colorPrimary: "#40513B",
+        },
+      }}
+    >
+      <App />
     </ConfigProvider>
-  </React.StrictMode>
+  </Provider>
 );
-
 
 reportWebVitals();
