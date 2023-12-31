044e412a0b00c0daf879869f4d31fd82156909b4
Shubham Kumar Pathak <shubhampathak26092002@gmail.com>
2024-01-01 03:04:22 +0530

    Layout of home page

--- client/src/components/ProctectedPage.js
+++ client/src/components/ProctectedPage.js
@@ -21,22 +21,35 @@ function ProctectedPage({ children }) {
     }
   };
   useEffect(() => {
-    if(localStorage.getItem("token")){
+    if (localStorage.getItem("token")) {
       validateToken();
-    }else{
+    } else {
       navigate("/login");
     }
   }, []);
 
   return (
-    <div>
-      {user && (
-        <div className="p-5">
-          {user.name}
-          {children}
+    user && (
+      <div>
+        {/*Header*/}
+        <div className="flex justify-between items-center bg-primary p-5">
+          <h1 className="text-2xl text-white">MarketPlace</h1>
+          <div className="bg-white py-2 px-5 rounded flex gap-1 items-center">
+            <i className="ri-shield-user-line"></i>
+            <span className="underline cursor-pointer uppercase">{user.name}</span>
+            <i
+              className="ri-logout-box-r-line ml-10"
+              onClick={() => {
+                localStorage.removeItem("token");
+                navigate("/login");
+              }}
+            ></i>
+          </div>
         </div>
-      )}
-    </div>
+        {/*Body*/}
+        <div className="p-5">{children}</div>
+      </div>
+    )
   );
 }
 
