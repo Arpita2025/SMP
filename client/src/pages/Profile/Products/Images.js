b3768b74998dcfd7e86f66995154fd04145d1993
arpita <arpita2002as@gmail.com>
2024-01-04 22:11:26 +0530

    delete product

--- client/src/pages/Profile/Products/Images.js
+++ client/src/pages/Profile/Products/Images.js
@@ -2,7 +2,7 @@ import { Upload, Button, message } from "antd";
 import React from "react";
 import { useDispatch } from "react-redux";
 import { SetLoader } from "../../../redux/loadersSlice";
-import { UploadProductImage } from "../../../apicalls/products";
+import { EditProduct, UploadProductImage } from "../../../apicalls/products";
 
 function Images({ selectedProduct, setShowProductForm, getData }) {
   const [showPreview = false, setShowPreview] = React.useState(true);
@@ -32,10 +32,41 @@ function Images({ selectedProduct, setShowProductForm, getData }) {
       message.error(error.message);
     }
   };
-
+  const deleteImage =async(image)=>{
+    try {
+      const updatedImageArray =images.filter((img)=> img!==image);
+      const updatedProduct ={...selectedProduct,images:updatedImageArray};
+      const response =await EditProduct(
+        selectedProduct._id,
+        updatedProduct
+      );
+      if(response.success){
+        message.success(response.message);
+        setImages(updatedProduct);
+        getData();
+      }else{
+        throw new Error(response.message);
+      }
+      dispatch(SetLoader(true));
+    } catch (error) {
+      dispatch(SetLoader(false));
+      message.error(error.message);
+    }
+  }
   return (
     <div>
-      <Upload
+      
+        <div className="flex gap-5  mb-5">
+          {images.map((image) => {
+            return (
+              <div className="flex gap-2 border-solid border-gray-500 rounded p-3 items-end">
+                <img className="h-20 w-20 object-cover" src={image} alt="" />
+                <i className="ri-delete-bin-line" onClick={() => {deleteImage(image)}}></i>
+              </div>
+            );
+          })}
+        </div>
+        <Upload
         listType="picture"
         beforeUpload={() => false}
         onChange={(info) => {
@@ -44,17 +75,6 @@ function Images({ selectedProduct, setShowProductForm, getData }) {
         }}
         showUploadList={showPreview}
       >
-        <div className="flex gap-5  mb-5">
-          {images.map((image) => {
-            return (
-              <div className="flex gap-2 border-solid border-gray-500 rounded p-3 items-end">
-                <img className="h-20 w-20 object-cover" src={image} alt="" />
-                <i className="ri-delete-bin-line" onClick={() => {}}></i>
-              </div>
-            );
-          })}
-        </div>
-
         <Button type="dashed">Upload Image</Button>
       </Upload>
       <div className="flex justify-end gap-5 mt-5">
